218f2a4c451ae7156701520035e6ac398bce8139
vukn95 <v.novakovic@levi9.com>
2021-07-03 09:53:20 +0200

    01-movement-added character, animator and click to move with camera following

--- Tierra del Fuego/Assets/Scripts/Mover.cs
+++ Tierra del Fuego/Assets/Scripts/Mover.cs
@@ -7,15 +7,30 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    Ray lastRay;
-
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        UpdateAnimator();
         
-        GetComponent<NavMeshAgent>().destination = target.position; 
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit){
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+    
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
